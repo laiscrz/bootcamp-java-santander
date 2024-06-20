7fb0462d599a19bf303544519abbfc23cd543bb4
laiscrz <lais.albino2003@gmail.com>
2024-06-20 18:55:13 -0300

    feat: code done from the class 'Anatomy of Classes II'

---
+++ JavaBasic/AnatomyClassJava/src/NomenclaturaVariaveis.java
@@ -0,0 +1,32 @@
+
+public class NomenclaturaVariaveis {
+
+    public static void main(String[] args) {
+
+        // Boa prática: usar camelCase para nomes de variáveis
+        String brasilNovo = ""; // Nome descritivo usando camelCase para uma variável
+
+        // Boa prática: usar palavras completas e significativas para constantes
+        final String BR = "Brasil"; // Constantes normalmente são escritas em letras maiúsculas
+
+        // Boa prática: nomear variáveis de forma descritiva
+        int anoAtual = 2021; // Nome significativo para o ano atual
+        anoAtual = 2022; // Atualizando o valor de anoAtual
+
+        System.out.println(brasilNovo);
+        System.out.println(BR);
+        System.out.println(anoAtual);
+
+        // Declaração válida de veriáveis
+        int numero$um = 1;
+        int numero1 = 1;
+        int numeroum = 1;
+        int longo = 1;
+
+        System.out.println(numero$um);
+        System.out.println(numero1);
+        System.out.println(numeroum);
+        System.out.println(longo);
+
+    }
+}
