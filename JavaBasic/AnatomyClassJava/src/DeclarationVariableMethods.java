4bee72d6f809f8ad98540f41222f2094faa00d19
laiscrz <lais.albino2003@gmail.com>
2024-06-20 20:22:36 -0300

    feat: code done from the class 'Anatomy of Classes III'

---
+++ JavaBasic/AnatomyClassJava/src/DeclarationVariableMethods.java
@@ -0,0 +1,35 @@
+
+public class DeclarationVariableMethods {
+
+    public static void main(String[] args) {
+
+        // Declaração de Var
+
+        String meuPais = "Brasil";
+
+        int anoFabricacao = 2024;
+
+        boolean verdadeira = false; // true / false
+
+        anoFabricacao = 2022;
+
+        System.out.println(meuPais);
+        System.out.println(anoFabricacao);
+        System.out.println(verdadeira);
+
+        // Declaração de Metodos
+        String nome = "Lais";
+
+        String sobrenome = "Silva";
+
+        String nomeCompleto = nomeCompleto(nome, sobrenome);
+
+        System.out.println(nomeCompleto);
+
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+
+        return "Resultado: " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
