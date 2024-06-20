6e6fbc4b2985037aa875b041191f9aa4755ec1ad
laiscrz <lais.albino2003@gmail.com>
2024-06-20 17:04:12 -0300

    feat: code done from the class 'Anatomy of Classes I'

---
+++ JavaBasic/AnatomyClassJava/src/MyClass.java
@@ -0,0 +1,26 @@
+// Nome da classe deve começar com letra maiúscula e seguir o padrão CamelCase
+public class MyClass {
+
+    // Main method - necessário se a classe for executável
+    public static void main(String[] args) {
+        // public: Modificador de acesso, indica que o método pode ser acessado de fora
+        // da classe.
+        // static: Método pertence à classe em si e não a instâncias específicas da
+        // classe.
+        // void: O método não retorna nenhum valor.
+        // main: Nome do método principal que é executado quando a classe é iniciada.
+        // (String[] args): Parâmetro do método, um array de strings que pode ser
+        // passado quando o programa é executado.
+
+        // Código dentro do método main
+
+        System.out.println("Hello World !");
+
+        // System: Classe do pacote java.lang que fornece acesso ao sistema e suas
+        // propriedades.
+        // out: Objeto do tipo PrintStream dentro da classe System, usado para saída
+        // padrão.
+        // println: Método de PrintStream usado para imprimir uma linha de texto seguida
+        // por uma quebra de linha.
+    }
+}
