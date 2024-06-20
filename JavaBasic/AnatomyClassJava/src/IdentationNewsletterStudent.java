f813d9b5b406bad66c1ae6ed9091093541c77536
laiscrz <lais.albino2003@gmail.com>
2024-06-20 20:41:38 -0300

    feat: code done from the class 'Anatomy of Classes IV'

---
+++ JavaBasic/AnatomyClassJava/src/IdentationNewsletterStudent.java
@@ -0,0 +1,15 @@
+
+public class IdentationNewsletterStudent {
+
+    public static void main(String[] args) {
+
+        int mediaFinal = 6;
+        if (mediaFinal < 6)
+            System.out.println("REPROVADO");
+        else if (mediaFinal == 6)
+            System.out.println("EXAME");
+        else
+            System.out.println("APROVADO");
+
+    }
+}
