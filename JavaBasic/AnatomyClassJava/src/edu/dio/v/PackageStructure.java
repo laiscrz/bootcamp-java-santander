e9264b2db35c6fb3fb4b018c0a362ace23d8164b
laiscrz <lais.albino2003@gmail.com>
2024-06-20 21:05:00 -0300

    feat: code done from the class 'Anatomy of Classes V'

---
+++ JavaBasic/AnatomyClassJava/src/edu/dio/v/PackageStructure.java
@@ -0,0 +1,11 @@
+package edu.dio.v;
+
+public class PackageStructure {
+
+    public static void main(String[] args) {
+        
+        // Criado estrutura em package
+        // Exemplo:
+        // package edu.dio.v;
+    }
+}
