1f51186273eab812cdd989be77ce1c2dd5fd8981
laiscrz <lais.albino2003@gmail.com>
2024-06-22 16:23:00 -0300

    feat: Project ExerciseSmartTV done

--- JavaBasic/Syntax/ExerciseSmartTV/src/edu/dio/MainUser.java
+++ JavaBasic/Syntax/ExerciseSmartTV/src/edu/dio/MainUser.java
@@ -11,6 +11,58 @@ public class MainUser {
         System.out.println("TV ligada? " + smartTV.ligada);
         System.out.println("Canal: " + smartTV.canal);
         System.out.println("Volume: " + smartTV.volume);
+        System.out.println("----------------------------------");
+
+        // Ligar a TV
+        System.out.println("Ligando a TV...");
+        smartTV.ligarTV();
+        System.out.println("TV ligada? " + smartTV.ligada);
+        System.out.println("----------------------------------");
+
+        // Aumentar o volume 3 vezes
+        System.out.println("Aumentando o volume...");
+        smartTV.aumentarVolume();
+        smartTV.aumentarVolume();
+        smartTV.aumentarVolume();
+        System.out.println("Volume: " + smartTV.volume);
+        System.out.println("----------------------------------");
+
+        // Diminuir o volume 1 vez
+        System.out.println("Diminuindo o volume...");
+        smartTV.diminuirVolume();
+        System.out.println("Volume: " + smartTV.volume);
+        System.out.println("----------------------------------");
+
+        // Mudar para o próximo canal
+        System.out.println("Mudando para o próximo canal...");
+        smartTV.mudarCanalProximo();
+        System.out.println("Canal: " + smartTV.canal);
+        System.out.println("----------------------------------");
+
+        // Mudar para o canal anterior
+        System.out.println("Mudando para o canal anterior...");
+        smartTV.mudarCanalAnterior();
+        System.out.println("Canal: " + smartTV.canal);
+        System.out.println("----------------------------------");
+
+        // Mudar para um canal específico
+        int canalEscolhido = 5;
+        System.out.println("Mudando para o canal " + canalEscolhido + "...");
+        smartTV.mudarCanalEscolhido(canalEscolhido);
+        System.out.println("Canal: " + smartTV.canal);
+        System.out.println("----------------------------------");
+
+        // Desligar a TV
+        System.out.println("Desligando a TV...");
+        smartTV.desligarTV();
+        System.out.println("TV ligada? " + smartTV.ligada);
+
+        // Visualizando estado final
+        System.out.println("-----------------STATUS FINAL-----------------");
+        System.out.println("TV ligada? " + smartTV.ligada);
+        System.out.println("Canal: " + smartTV.canal);
+        System.out.println("Volume: " + smartTV.volume);
+        System.out.println("----------------------------------");
 
     }
 }
--- JavaBasic/Syntax/ExerciseSmartTV/src/edu/dio/SmartTV.java
+++ JavaBasic/Syntax/ExerciseSmartTV/src/edu/dio/SmartTV.java
@@ -7,9 +7,10 @@ public class SmartTV {
     int canal = 1;
     int volume = 10;
 
+    // MÉTODOS -> LIGADA (BOOLEAN)
     // Método para ligar a TV
-    public void ligarTV () {
-       ligada = true;
+    public void ligarTV() {
+        ligada = true;
     };
 
     // Método para desligar a TV
@@ -17,11 +18,30 @@ public class SmartTV {
         ligada = false;
     };
 
+    // MÉTODOS -> VOLUME (INT)
     // Método para aumentar o volume
+    public void aumentarVolume() {
+        volume++;
+    };
 
     // Método para diminuir o volume
+    public void diminuirVolume() {
+        volume--;
+    };
 
+    // MÉTODOS -> CANAL (INT)
     // Método para mudar para um canal específico
+    public void mudarCanalEscolhido(int canalEscolhido){
+        canal = canalEscolhido;
+    };
 
     // Método para mudar para o próximo canal
+    public void mudarCanalProximo(){
+        canal++;
+    }
+
+    // Método para mudar para o canal anterior
+    public void mudarCanalAnterior() {
+        canal--;
+    }
 }
