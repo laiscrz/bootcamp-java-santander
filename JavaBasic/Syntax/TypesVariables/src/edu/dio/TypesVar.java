c2ac2cde347c90614ed982a8ad6ad213850ab89d
laiscrz <lais.albino2003@gmail.com>
2024-06-21 21:00:40 -0300

    chore: update project

---
+++ JavaBasic/Syntax/TypesVariables/src/edu/dio/TypesVar.java
@@ -0,0 +1,43 @@
+package edu.dio;
+
+public class TypesVar {
+
+    public static void main(String[] args) {
+
+        // tipos primitivos -> int, byte, short, long, float, double, boolean e char 
+
+        // Tipo int
+        int inteiro = 123;
+        System.out.println("int: " + inteiro);
+
+        // Tipo byte
+        byte b = 10;
+        System.out.println("byte: " + b);
+
+        // Tipo short
+        short s = 1000;
+        System.out.println("short: " + s);
+
+        // Tipo long
+        long l = 100000L;
+        System.out.println("long: " + l);
+
+        // Tipo float
+        float f = 12.34f;
+        System.out.println("float: " + f);
+
+        // Tipo double
+        double d = 123.456;
+        System.out.println("double: " + d);
+
+        // Tipo boolean
+        boolean bool = true;
+        System.out.println("boolean: " + bool);
+
+        // Tipo char
+        char c = 'A';
+        System.out.println("char: " + c);
+
+
+    }
+}
