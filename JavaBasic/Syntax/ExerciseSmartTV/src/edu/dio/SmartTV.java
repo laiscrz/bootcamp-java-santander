bfe7c5c4f7438f7bd7f97eb3e2571f1b57436572
laiscrz <lais.albino2003@gmail.com>
2024-06-22 15:57:40 -0300

    chore: update class SmartTV.java

--- JavaBasic/Syntax/ExerciseSmartTV/src/edu/dio/SmartTV.java
+++ JavaBasic/Syntax/ExerciseSmartTV/src/edu/dio/SmartTV.java
@@ -2,5 +2,26 @@ package edu.dio;
 
 public class SmartTV {
 
-   
+    // características: ligada (boolean), canal (int) e volume (int);
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 10;
+
+    // Método para ligar a TV
+    public void ligarTV () {
+       ligada = true;
+    };
+
+    // Método para desligar a TV
+    public void desligarTV() {
+        ligada = false;
+    };
+
+    // Método para aumentar o volume
+
+    // Método para diminuir o volume
+
+    // Método para mudar para um canal específico
+
+    // Método para mudar para o próximo canal
 }
