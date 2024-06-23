16843bb9d3d6e64a0b8dcf04edbcea65aa543e66
laiscrz <lais.albino2003@gmail.com>
2024-06-23 14:50:10 -0300

    feat: update class ConditionalChained

--- JavaBasic/ConditionalStructure/TypeFluxControl/src/ConditionalChained.java
+++ JavaBasic/ConditionalStructure/TypeFluxControl/src/ConditionalChained.java
@@ -1,3 +1,12 @@
 public class ConditionalChained {
-    
+    public static void main(String[] args) {
+        int nota = 6;
+
+        if (nota >= 7)
+            System.out.println("Aprovado");
+        else if (nota >= 5 && nota < 7)
+            System.out.println("Recuperação");
+        else
+            System.out.println("Reprovado");
+    }
 }
