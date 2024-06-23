7dcac5e5e503e30b33fcded530a05430a8a85e5b
laiscrz <lais.albino2003@gmail.com>
2024-06-23 12:13:32 -0300

    feat: code done from the class 'Terminal and arguments II'

--- JavaBasic/Syntax/TerminalJava/src/TestTerminal.java
+++ JavaBasic/Syntax/TerminalJava/src/TestTerminal.java
@@ -1,13 +1,15 @@
 
-
 public class TestTerminal {
 
     public static void main(String[] args) {
         
-        System.out.println("Hello, welcome to the terminal !");
+        // Mensagem de boas-vindas exibida ao iniciar o programa
+        System.out.println("Hello, welcome to the terminal!");
 
-        // abre terminal
-        // acesse a pasta
-        // comandos -> java TestTerminal
+        // Para executar este programa via terminal:
+        // 1. Abra o terminal do seu sistema operacional.
+        // 2. Navegue até o diretório onde este arquivo Java (TestTerminal.java) está localizado.
+        // 3. Compile o arquivo Java usando o comando: javac TestTerminal.java
+        // 4. Após compilar com sucesso, execute o programa Java usando o comando: java TestTerminal
     }
 }
