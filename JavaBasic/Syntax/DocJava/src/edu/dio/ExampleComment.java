242ae5871a4e461e172a32870e18faee04dd667a
laiscrz <lais.albino2003@gmail.com>
2024-06-23 11:52:10 -0300

    feat: code done from the class 'Java Doc'

--- JavaBasic/Syntax/DocJava/src/edu/dio/ExampleComment.java
+++ JavaBasic/Syntax/DocJava/src/edu/dio/ExampleComment.java
@@ -14,14 +14,14 @@ public class ExampleComment {
          * mais detalhes sobre o que o código faz. Pode ser usado para descrever
          * a lógica do programa, as variáveis usadas, ou qualquer outro aspecto
          * que necessite de uma explicação mais longa.
-         */
+        */
         int x = 10; // Inicializa a variável x com o valor 10
         int y = 20; // Inicializa a variável y com o valor 20
 
         /*
          * Este bloco de código abaixo realiza a soma das variáveis x e y,
          * e armazena o resultado na variável z.
-         */
+        */
         int z = x + y; // z agora tem o valor 30
 
         // Imprime o resultado da soma
