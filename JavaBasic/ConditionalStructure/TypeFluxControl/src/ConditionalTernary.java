c199bd88c76b155ef53bdec215333f4b0a83bc4a
laiscrz <lais.albino2003@gmail.com>
2024-06-23 14:59:15 -0300

    feat: update class ConditionalTernary

--- JavaBasic/ConditionalStructure/TypeFluxControl/src/ConditionalTernary.java
+++ JavaBasic/ConditionalStructure/TypeFluxControl/src/ConditionalTernary.java
@@ -2,5 +2,14 @@ public class ConditionalTernary {
 
     public static void main(String[] args) {
 
+        // Cenario 1
+        int nota = 7;
+        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+        System.out.println(resultado);
+
+        // Cenario 2
+        int not = 6;
+        String result = not >= 7 ? "Aprovado" : not >= 5 && not < 7 ? "Recuperação" : "Reprovado";
+        System.out.println(result);
     }
 }
