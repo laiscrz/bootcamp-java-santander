df0dd0f9c6649fa49d8ff776eb6b70a29835b717
laiscrz <lais.albino2003@gmail.com>
2024-06-23 14:34:21 -0300

    feat: code done from the class 'Type Flux Control'

--- JavaBasic/ConditionalStructure/TypeFluxControl/src/edu/dio/TestConditional.java
+++ JavaBasic/ConditionalStructure/TypeFluxControl/src/edu/dio/TestConditional.java
@@ -1,5 +1,14 @@
 package edu.dio;
 
 public class TestConditional {
-    
+    public static void main(String[] args) {
+
+        int idade = 20;
+
+        if (idade >= 18) {
+            System.out.println("Você é maior de idade.");
+        } else {
+            System.out.println("Você é menor de idade.");
+        }
+    }
 }
