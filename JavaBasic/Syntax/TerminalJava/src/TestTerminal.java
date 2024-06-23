722fcc34e0605f596c31967e7fbf01cd425094d0
laiscrz <lais.albino2003@gmail.com>
2024-06-23 12:12:40 -0300

    feat: code done from the class 'Terminal and arguments I'

---
+++ JavaBasic/Syntax/TerminalJava/src/TestTerminal.java
@@ -0,0 +1,13 @@
+
+
+public class TestTerminal {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello, welcome to the terminal !");
+
+        // abre terminal
+        // acesse a pasta
+        // comandos -> java TestTerminal
+    }
+}
