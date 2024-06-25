54bc4913969a3ef4b0c67da41664dbb0427f1345
laiscrz <lais.albino2003@gmail.com>
2024-06-25 19:33:33 -0300

    feat: update class LoopForEach

--- Content/JavaBasic/StructureRepetition/TypesLoops/src/LoopForEach.java
+++ Content/JavaBasic/StructureRepetition/TypesLoops/src/LoopForEach.java
@@ -1,4 +1,14 @@
 
 public class LoopForEach {
 
+    public static void main(String[] args) {
+        String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+
+        // Forma abreviada
+        for (String aluno : alunos) {
+            System.out.println(aluno);
+        }
+
+    }
+
 }
