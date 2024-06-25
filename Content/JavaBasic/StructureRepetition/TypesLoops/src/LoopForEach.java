6e310ee777f47120b169d0787888f6e0957d5ff5
laiscrz <lais.albino2003@gmail.com>
2024-06-25 19:37:02 -0300

    feat: class LoopForEach done

--- Content/JavaBasic/StructureRepetition/TypesLoops/src/LoopForEach.java
+++ Content/JavaBasic/StructureRepetition/TypesLoops/src/LoopForEach.java
@@ -1,14 +1,24 @@
-
 public class LoopForEach {
 
     public static void main(String[] args) {
-        String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+        // ESTRUTURA DE REPETIÇÃO -> FOREACH
 
-        // Forma abreviada
+        // Array de exemplo contendo nomes de alunos
+        String[] alunos = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+
+        // Iteração utilizando o foreach
         for (String aluno : alunos) {
-            System.out.println(aluno);
+            /*
+             * Sintaxe do foreach:
+             * 
+             * for (tipo elemento : colecao) {
+             *      // Código a ser executado para cada elemento
+             * }
+             * 
+             * Neste caso, 'aluno' é o elemento atual sendo percorrido no array 'alunos'.
+             * A cada iteração, o nome do aluno é exibido no console.
+             */
+            System.out.println("O nome do Aluno(a) é: " + aluno);
         }
-
     }
-
 }
