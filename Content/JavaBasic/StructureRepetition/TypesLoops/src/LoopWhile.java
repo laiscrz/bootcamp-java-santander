24f2825dcbffecb7803cc95fe142c32c8cfaa5f8
laiscrz <lais.albino2003@gmail.com>
2024-06-25 20:00:11 -0300

    feat: update class LoopWhile

--- Content/JavaBasic/StructureRepetition/TypesLoops/src/LoopWhile.java
+++ Content/JavaBasic/StructureRepetition/TypesLoops/src/LoopWhile.java
@@ -1,4 +1,19 @@
 
-public class LoopWhile {
 
+public class LoopWhile {
+    public static void main(String[] args) {
+
+        // ESTRUTURA DE REPETIÇÃO -> WHILE
+
+        /**
+         * Estrutura do loop while:
+         * 
+         * while (condição) {
+         * // bloco de código a ser repetido
+         * // comando que será executado até que a expressão de validação torne-se falsa
+         * }
+        */
+
+
+    }
 }
