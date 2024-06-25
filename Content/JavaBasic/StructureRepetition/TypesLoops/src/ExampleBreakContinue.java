6e7eb9f55cbdf220fcd8ba15cb07a7fd962af55b
laiscrz <lais.albino2003@gmail.com>
2024-06-25 19:44:48 -0300

    feat: class ExampleBreakContinue done

--- Content/JavaBasic/StructureRepetition/TypesLoops/src/ExampleBreakContinue.java
+++ Content/JavaBasic/StructureRepetition/TypesLoops/src/ExampleBreakContinue.java
@@ -1,20 +1,26 @@
-
 public class ExampleBreakContinue {
 
     public static void main(String[] args) {
-        // Break
+        // Uso do break
         System.out.println("-----------Uso Break-----------");
         for (int numero = 0; numero <= 5; numero++) {
             if (numero == 3)
-                break;
+                break; // Interrompe o loop quando numero é igual a 3
 
-            System.out.println(numero);
-
+            System.out.println(numero); // Imprime o valor atual de numero
         }
 
-        
-       
+        // Uso do continue
+        System.out.println("-----------Uso Continue-----------");
+        for (int numero = 1; numero <= 5; numero++) {
+            if (numero == 3)
+                continue; // Pula para a próxima iteração quando numero é igual a 3
 
+            System.out.println(numero); // Imprime o valor atual de numero, exceto quando numero é igual a 3
+        }
     }
+
+    // Observem que sempre o break e continue ``, está condicionado a um critério de
+    // negócio.
 
 }
