3abba9838c63353cf0b77fa8ecb9df51d60fad32
laiscrz <lais.albino2003@gmail.com>
2024-06-25 19:08:19 -0300

    feat: update class LoopFor

--- Content/JavaBasic/StructureRepetition/TypesLoops/src/LoopFor.java
+++ Content/JavaBasic/StructureRepetition/TypesLoops/src/LoopFor.java
@@ -2,6 +2,25 @@
 public class LoopFor {
 
     public static void main(String[] args) {
-        
+
+        // ESTRUTURA REPETIÇÃO -> FOR
+
+        // Exemplo FOR
+
+        for(int carneirinhos = 1 ; carneirinhos <=20; carneirinhos ++) {
+			System.out.println(carneirinhos + " - Carneirinho(s)");
+		};
+
+        /*
+         * estrutura do controle de fluxo for
+         * 
+         * for (bloco de inicialização; expressão booleana de validação; bloco de
+         * atualização)
+         * {
+         * // comando que será executado até que a
+         * // expressão de validação torne-se falsa
+         * }
+        */
+
     }
 }
