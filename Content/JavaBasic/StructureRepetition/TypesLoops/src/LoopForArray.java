4b0b0ae3cdcd4175bedd1061df8ac72dbb8387f9
laiscrz <lais.albino2003@gmail.com>
2024-06-25 19:18:34 -0300

    feat: class LoopForArray done

--- Content/JavaBasic/StructureRepetition/TypesLoops/src/LoopForArray.java
+++ Content/JavaBasic/StructureRepetition/TypesLoops/src/LoopForArray.java
@@ -2,8 +2,10 @@
 public class LoopForArray {
 
     public static void main(String[] args) {
+        // array do tipo String
         String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
 
+        // Itera sobre o array imprindo cada indice
         for (int x = 0; x < alunos.length; x++) {
             System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
         }
