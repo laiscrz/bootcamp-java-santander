a7ee3ceb9f35997e54289bb5319b10804f72f4e3
laiscrz <lais.albino2003@gmail.com>
2024-06-25 19:42:56 -0300

    feat: update class ExampleBreakContinue

--- Content/JavaBasic/StructureRepetition/TypesLoops/src/ExampleBreakContinue.java
+++ Content/JavaBasic/StructureRepetition/TypesLoops/src/ExampleBreakContinue.java
@@ -1,4 +1,20 @@
 
 public class ExampleBreakContinue {
 
+    public static void main(String[] args) {
+        // Break
+        System.out.println("-----------Uso Break-----------");
+        for (int numero = 0; numero <= 5; numero++) {
+            if (numero == 3)
+                break;
+
+            System.out.println(numero);
+
+        }
+
+        
+       
+
+    }
+
 }
