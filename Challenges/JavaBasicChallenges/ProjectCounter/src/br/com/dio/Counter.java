124341b928f37d8a4610e85006d9ba26ef6ce768
laiscrz <lais.albino2003@gmail.com>
2024-06-26 18:40:23 -0300

    feat: ProjectCounter done

--- Challenges/JavaBasicChallenges/ProjectCounter/src/br/com/dio/Counter.java
+++ Challenges/JavaBasicChallenges/ProjectCounter/src/br/com/dio/Counter.java
@@ -15,17 +15,29 @@ public class Counter {
         int parameterTwo = scanner.nextInt();
 
         try {
-
+            count(parameterOne, parameterTwo);
+        } catch (InvalidParametersException e) {
+            System.out.println(e.getMessage());
         } catch (Exception e) {
 
         }
+
+        scanner.close();
     }
 
     static void count(int parameterOne, int parameterTwo) throws InvalidParametersException {
         // validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+        if (parameterOne >= parameterTwo) {
+            throw new InvalidParametersException("O segundo parâmetro deve ser maior que o primeiro");
+        }
 
         // int contagem = parametroDois - parametroUm;
+        int counts = parameterTwo - parameterOne;
+
         // realizar o for para imprimir os números com base na variável contagem
+        for (int i = 1; i <= counts; i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
 
     }
 }
