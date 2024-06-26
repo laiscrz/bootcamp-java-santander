f4b3bbfb629e5723d10b33cf7add4be17656d93b
laiscrz <lais.albino2003@gmail.com>
2024-06-26 17:36:38 -0300

    chore: update ProjectCases

---
+++ Challenges/JavaBasicChallenges/ProjectCasesFluxControl/src/br/com/dio/cases/i/ProcessSelective.java
@@ -0,0 +1,12 @@
+package br.com.dio.cases.i;
+
+public class ProcessSelective {
+
+    // Constante do Salario Base
+    private static final double SALARIO_BASE = 2000.00;
+
+    public static void main(String[] args) {
+
+        
+    }
+}
