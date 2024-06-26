e4450b0f0942e216c4a2393fbdfc3c864ea1b325
laiscrz <lais.albino2003@gmail.com>
2024-06-26 11:32:52 -0300

    feat: class CustomExceptions done

--- Content/JavaBasic/HandlingExceptions/ProjectJavaException/src/CustomExceptions.java
+++ Content/JavaBasic/HandlingExceptions/ProjectJavaException/src/CustomExceptions.java
@@ -1,3 +1,22 @@
-public class CustomExceptions {
+import exceptions.CepInvalidoException;
 
+public class CustomExceptions {
+    public static void main(String[] args) {
+        String cep = "23765064";
+        
+        try {
+            String cepFormatado = formatarCep(cep);
+            System.out.println("CEP formatado: " + cepFormatado);
+        } catch (CepInvalidoException e) {
+            System.out.println("Erro: CEP inválido.");
+        }
+    }
+    
+    static String formatarCep(String cep) throws CepInvalidoException {
+        if (cep.length() != 8)
+            throw new CepInvalidoException();
+        
+        // Simulando um CEP formatado
+        return "23.765-064";
+    }
 }
