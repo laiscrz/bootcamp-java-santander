e9ebbb27c7b0073bcde1c2fcdfde22f3373cc5db
laiscrz <lais.albino2003@gmail.com>
2024-06-26 17:45:06 -0300

    feat: 'Cases II' done

--- Challenges/JavaBasicChallenges/ProjectCasesFluxControl/src/br/com/dio/ProcessSelectiveII.java
+++ Challenges/JavaBasicChallenges/ProjectCasesFluxControl/src/br/com/dio/ProcessSelectiveII.java
@@ -1,5 +1,42 @@
 package br.com.dio;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessSelectiveII {
 
+    // Constante para o salário base
+    private static final double SALARIO_BASE = 2000.00;
+    // Array com a lista de candidatos
+    private static final String[] CANDIDATOS = { "FELIPE", "MÁRCIA", "JULIA", "PAULO", "AUGUSTO", "MÔNICA", "FABRÍCIO",
+            "MIRELA", "DANIELA", "JORGE" };
+    // Limite máximo de candidatos selecionados
+    private static final int LIMITE_CANDIDATOS = 5;
+
+    public static void main(String[] args) {
+
+        int candidatosSelecionados = 0;
+
+        for (String candidato : CANDIDATOS) {
+            if (candidatosSelecionados >= LIMITE_CANDIDATOS) {
+                break;
+            }
+
+            double salarioPretendido = valorPretendido();
+            System.out.println(candidato + " solicitou um salário de R$ " + salarioPretendido);
+
+            if (salarioPretendido <= SALARIO_BASE) {
+                System.out.println("LIGAR PARA O CANDIDATO: " + candidato);
+                candidatosSelecionados++;
+            } else {
+                System.out.println("AGUARDANDO RESULTADO DOS DEMAIS CANDIDATOS: " + candidato);
+            }
+        }
+
+        System.out.println("Total de candidatos selecionados para entrevista: " + candidatosSelecionados);
+    }
+
+    // Método que simula o valor pretendido
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+    }
 }
