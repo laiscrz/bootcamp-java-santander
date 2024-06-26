28ef0579f68046a26044a9ffc7933d9d278cf19c
laiscrz <lais.albino2003@gmail.com>
2024-06-26 10:44:12 -0300

    feat: class TestExceptions done

--- Content/JavaBasic/HandlingExceptions/ProjectJavaException/src/TestExceptions.java
+++ Content/JavaBasic/HandlingExceptions/ProjectJavaException/src/TestExceptions.java
@@ -1,10 +1,10 @@
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class TestExceptions {
 
     public static void main(String[] args) {
-        
         // Tratamento de Exceções
         /**
          * sintaxe:
@@ -20,24 +20,31 @@ public class TestExceptions {
         */
 
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
-        
-        System.out.println("Digite seu nome");
-        String nome = scanner.next();
-        
-        System.out.println("Digite seu sobrenome");
-        String sobrenome = scanner.next();
 
-        System.out.println("Digite sua idade");
-        int idade = scanner.nextInt();
-        
-        System.out.println("Digite sua altura");
-        double altura = scanner.nextDouble();
+        try {
+            System.out.println("Digite seu nome");
+            String nome = scanner.next();
 
-        
-        //imprimindo os dados obtidos pelo usuario
-        System.out.println("Olá, me chamo " + nome.toUpperCase() + " " + sobrenome.toUpperCase());
-        System.out.println("Tenho " + idade + " anos ");
-        System.out.println("Minha altura é " + altura + "cm ");
-        scanner.close();   
+            System.out.println("Digite seu sobrenome");
+            String sobrenome = scanner.next();
+
+            System.out.println("Digite sua idade");
+            int idade = scanner.nextInt();
+
+            System.out.println("Digite sua altura");
+            double altura = scanner.nextDouble();
+
+            // Imprimindo os dados obtidos pelo usuário
+            System.out.println("Olá, me chamo " + nome.toUpperCase() + " " + sobrenome.toUpperCase());
+            System.out.println("Tenho " + idade + " anos ");
+            System.out.println("Minha altura é " + altura + "cm ");
+        } catch (InputMismatchException e) {
+            System.out.println("Erro: Tipo de dado inválido. Por favor, insira um valor válido.");
+            scanner.nextLine(); // Limpar o buffer do scanner
+        } catch (Exception e) {
+            System.out.println("Ocorreu um erro inesperado: " + e.getMessage());
+        } finally {
+            scanner.close(); // Fechando o scanner no bloco finally para garantir que seja sempre executado
+        }
     }
 }
