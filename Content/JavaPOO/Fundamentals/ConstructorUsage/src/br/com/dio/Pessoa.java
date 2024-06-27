7a9926924a6553db87aa2cc9c497b4a295bd6f61
laiscrz <lais.albino2003@gmail.com>
2024-06-27 16:49:32 -0300

    feat: ConstructorUsage project done

--- Content/JavaPOO/Fundamentals/ConstructorUsage/src/br/com/dio/Pessoa.java
+++ Content/JavaPOO/Fundamentals/ConstructorUsage/src/br/com/dio/Pessoa.java
@@ -3,4 +3,42 @@ package br.com.dio;
 public class Pessoa {
 
     private String nome;
+    private String cpf;
+    private String endereco;
+
+    // construtor vazio (não parametrizado)
+    public Pessoa(){
+
+    }
+
+    // construtor cheio (parametrizado)
+    public Pessoa(String nome, String cpf, String endereco){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.endereco = endereco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
 }
--- Content/JavaPOO/Fundamentals/ConstructorUsage/src/br/com/dio/SistemaCadastro.java
+++ Content/JavaPOO/Fundamentals/ConstructorUsage/src/br/com/dio/SistemaCadastro.java
@@ -3,7 +3,25 @@ package br.com.dio;
 public class SistemaCadastro {
 
     public static void main(String[] args) {
+
+        // com construtor vazio (uso set)
+        Pessoa pessoaUm = new Pessoa();
+        pessoaUm.setNome("José");
+        pessoaUm.setCpf("12156332");
+        pessoaUm.setEndereco("Av Treze de Maio, n78");
+        // Visualize - Pessoa 1
+        System.out.println("-----------Pessoa 1-----------");
+        System.out.println("Nome: " + pessoaUm.getNome() +
+                "\nCPF: " + pessoaUm.getCpf() +
+                "\nEndereço: " + pessoaUm.getEndereco() + "\n");
         
-        
+        // usando construtor cheio
+        Pessoa pessoaDois = new Pessoa("Maria", "1225515", "Av Paulista, n28");
+        // Visualize - Pessoa 2
+        System.out.println("-----------Pessoa 2-----------");
+        System.out.println("Nome: " + pessoaDois.getNome() +
+                "\nCPF: " + pessoaDois.getCpf() +
+                "\nEndereço: " + pessoaDois.getEndereco() + "\n");
+
     }
 }
