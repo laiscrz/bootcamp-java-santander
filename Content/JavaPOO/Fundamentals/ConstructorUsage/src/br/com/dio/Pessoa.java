8915bcf9a78f076eaa9a6930e5ebd07321cc2995
laiscrz <lais.albino2003@gmail.com>
2024-06-27 16:35:06 -0300

    chore: Initial commit for ConstructorUsage project

---
+++ Content/JavaPOO/Fundamentals/ConstructorUsage/src/br/com/dio/Pessoa.java
@@ -0,0 +1,6 @@
+package br.com.dio;
+
+public class Pessoa {
+
+    private String nome;
+}
---
+++ Content/JavaPOO/Fundamentals/ConstructorUsage/src/br/com/dio/SistemaCadastro.java
@@ -0,0 +1,9 @@
+package br.com.dio;
+
+public class SistemaCadastro {
+
+    public static void main(String[] args) {
+        
+        
+    }
+}
