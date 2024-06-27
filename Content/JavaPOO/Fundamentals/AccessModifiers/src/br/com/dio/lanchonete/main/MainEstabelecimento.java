b2f4f40b7f01cc7f4e3ed03d15927e7dc511e793
laiscrz <lais.albino2003@gmail.com>
2024-06-27 13:02:14 -0300

    feat: update AccessModifiers project

---
+++ Content/JavaPOO/Fundamentals/AccessModifiers/src/br/com/dio/lanchonete/main/MainEstabelecimento.java
@@ -0,0 +1,60 @@
+package br.com.dio.lanchonete.main;
+
+import br.com.dio.lanchonete.model.Almoxarife;
+import br.com.dio.lanchonete.model.Atendente;
+import br.com.dio.lanchonete.model.Cliente;
+import br.com.dio.lanchonete.model.Cozinheiro;
+
+public class MainEstabelecimento {
+    public static void main(String[] args) {
+        System.out.println("---------Cozinheiro---------");
+        Cozinheiro cozinheiro = new Cozinheiro();
+        // ações que não precisam estarem disponíveis para toda a aplicação
+        cozinheiro.lavarIngredientes();
+        cozinheiro.baterVitaminaLiquidificador();
+        cozinheiro.selecionarIngredientesVitamina();
+        cozinheiro.prepararLanche();
+        cozinheiro.prepararVitamina();
+        cozinheiro.prepararVitamina();
+
+        // ações que estabelecimento precisa ter ciência
+        cozinheiro.adicionarSucoNoBalcao();
+        cozinheiro.adicionarLancheNoBalcao();
+        cozinheiro.adicionarComboNoBalcao();
+
+        System.out.println("---------Almoxarife---------");
+        Almoxarife almoxarife = new Almoxarife();
+        // ações que não precisam estarem disponíveis para toda a aplicação
+        almoxarife.controlarEntrada();
+        almoxarife.controlarSaida();
+        // ação que somente o seu pacote cozinha precisa conhecer (default)
+        almoxarife.entregarIngredientes();
+        almoxarife.trocarGas();
+
+        System.out.println("---------Atendente---------");
+        Atendente atendente = new Atendente();
+        atendente.pegarLancheCozinha();
+        atendente.receberPagamento();
+        atendente.servindoMesa();
+        // ação que somente o seu pacote cozinha precisa conhecer (default)
+        atendente.trocarGas();
+
+        System.out.println("---------Cliente---------");
+        Cliente cliente = new Cliente();
+        cliente.escolherLanche();
+        cliente.fazerPedido();
+        cliente.pagarConta();
+
+        // não deveria, mas o estabelecimento
+        // ainda não definiu normas de atendimento
+        cliente.pegarPedidoBalcao();
+
+        // esta ação é muito sigilosa, qua tal ser privada ?
+        cliente.consultarSaldoAplicativo();
+
+        // já pensou os clientes ouvindo que o gás acabou ?
+        cozinheiro.pedirParaTrocarGas(atendente);
+        cozinheiro.pedirParaTrocarGas(almoxarife);
+
+    }
+}
---
+++ Content/JavaPOO/Fundamentals/AccessModifiers/src/br/com/dio/lanchonete/model/Almoxarife.java
@@ -0,0 +1,20 @@
+package br.com.dio.lanchonete.model;
+
+public class Almoxarife {
+    public void controlarEntrada() {
+        System.out.println("CONTROLANDO A ENTRADA DOS ITENS");
+    }
+
+    public void controlarSaida() {
+        System.out.println("CONTROLANDO A SAIDA DOS ITENS");
+    }
+
+    public void entregarIngredientes() {
+        System.out.println("ENTREGANDO INGREDIENTES");
+        // ...?
+    }
+
+    public void trocarGas() {
+        System.out.println("ALMOXARIFE TROCANDO O GÁS");
+    }
+}
