4f3ebc38a9a42bbf3c75c9f6550836fe8dd82cdc
laiscrz <lais.albino2003@gmail.com>
2024-06-27 10:31:22 -0300

    feat:ClassObjects poject done

---
+++ Content/JavaPOO/Fundamentals/ClassObjects/src/School.java
@@ -0,0 +1,23 @@
+// Criando objetos a partir da classe Student
+
+public class School {
+    public static void main(String[] args) throws Exception {
+      Student student1 = new Student();
+      student1.name= "John";
+      student1.age= 12;
+      student1.color= "FAIR";
+      student1.sex= 'M';
+
+      Student student2 = new Student();
+      student2.name= "Sophia";
+      student2.age= 10;
+      student2.color= "FAIR";
+      student2.sex= 'F';
+
+      Student student3 = new Student();
+      student3.name= "Lily";
+      student3.age= 11;
+      student3.color= "FAIR";
+      student3.sex= 'F';
+    }
+}
---
+++ Content/JavaPOO/Fundamentals/ClassObjects/src/Student.java
@@ -0,0 +1,23 @@
+// Classe Student
+
+public class Student {
+
+    String name;
+    int age;
+    String color;
+    char sex;
+
+    // metodo
+    void eating(/*Lunch lunch*/) {
+        // NOSSO CÓDIGO AQUI
+    }
+
+    // metodo
+    void drinking(/*Juice juice*/) {
+        // NOSSO CÓDIGO AQUI
+    }
+
+    void running() {
+        // NOSSO CÓDIGO AQUI
+    }
+}
