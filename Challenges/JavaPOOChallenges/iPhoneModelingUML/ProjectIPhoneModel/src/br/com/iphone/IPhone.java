c20cda194494e34a3eeb061873894a68a11c777d
laiscrz <lais.albino2003@gmail.com>
2024-06-29 10:07:10 -0300

    chore: ProjectIPhoneModel project done

--- Challenges/JavaPOOChallenges/iPhoneModelingUML/ProjectIPhoneModel/src/br/com/iphone/IPhone.java
+++ Challenges/JavaPOOChallenges/iPhoneModelingUML/ProjectIPhoneModel/src/br/com/iphone/IPhone.java
@@ -1,8 +1,48 @@
 package br.com.iphone;
 
+import br.com.iphone.reprodutor.ReprodutorMusical;
+import br.com.iphone.dispositivos.AparelhoTelefonico;
+import br.com.iphone.componentes.NavegadorInternet;
+
 public class IPhone {
 
+    private ReprodutorMusical reprodutorMusical;
+    private AparelhoTelefonico aparelhoTelefonico;
+    private NavegadorInternet navegadorInternet;
+
+    public IPhone() {
+        this.reprodutorMusical = new ReprodutorMusical();
+        this.aparelhoTelefonico = new AparelhoTelefonico();
+        this.navegadorInternet = new NavegadorInternet();
+    }
+
     public static void main(String[] args) {
+        IPhone iphone = new IPhone();
+        iphone.iniciar();
+    }
+
+    void iniciar() {
+        System.out.println("*******Iniciando iPhone*******");
+        System.out.println("-----------------------------");
+        System.out.println("Abrindo Reprodutor Musical...");
+        reprodutorMusical.selecionarMusica("Sorry by Justin Bieber");
+        reprodutorMusical.tocar();
+        reprodutorMusical.pausar();
+        System.out.println("-----------------------------");
         
+        System.out.println("Abrindo Aparelho Telefonico...");
+        aparelhoTelefonico.ligar("123-456-7890");
+        aparelhoTelefonico.atender();
+        aparelhoTelefonico.iniciarCorreioVoz();
+        aparelhoTelefonico.encerrar();
+        System.out.println("-----------------------------");
+        
+        System.out.println("Abrindo Navegador de Internet...");
+        navegadorInternet.adicionarNovaAba();
+        navegadorInternet.exibirPagina("www.apple.com");
+        navegadorInternet.atualizarPagina();
+        System.out.println("-----------------------------");
+        
+        System.out.println("*******Encerrando iPhone*******");
     }
 }
