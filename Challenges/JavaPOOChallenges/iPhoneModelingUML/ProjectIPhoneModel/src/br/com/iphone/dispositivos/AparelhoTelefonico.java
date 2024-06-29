eb64d44333f59ba4c53cedc54e243f0b34ff2ab3
laiscrz <lais.albino2003@gmail.com>
2024-06-28 23:54:46 -0300

    feat: update package 'dispositivos'

--- Challenges/JavaPOOChallenges/iPhoneModelingUML/ProjectIPhoneModel/src/br/com/iphone/dispositivos/AparelhoTelefonico.java
+++ Challenges/JavaPOOChallenges/iPhoneModelingUML/ProjectIPhoneModel/src/br/com/iphone/dispositivos/AparelhoTelefonico.java
@@ -1,5 +1,21 @@
-package br.com.iphone.componentes;
+package br.com.iphone.dispositivos;
 
-public class AparelhoTelefonico {
+public class AparelhoTelefonico extends DispositivoComunicacao {
+
+    @Override
+    public void ligar(String numero) {
+        System.out.println("Ligando para " + numero);
+        System.out.println("Chamando...");
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo chamada...");
+    }
+
+    public void encerrar() {
+        System.out.println("Encerrando chamada...");
+        System.out.println("Chamada encerrada.");
+    }
 
 }
--- Challenges/JavaPOOChallenges/iPhoneModelingUML/ProjectIPhoneModel/src/br/com/iphone/dispositivos/DispositivoComunicacao.java
+++ Challenges/JavaPOOChallenges/iPhoneModelingUML/ProjectIPhoneModel/src/br/com/iphone/dispositivos/DispositivoComunicacao.java
@@ -2,4 +2,14 @@ package br.com.iphone.dispositivos;
 
 public abstract class DispositivoComunicacao {
 
+    public abstract void ligar(String numero);
+
+    public abstract void atender();
+
+    public abstract void encerrar();
+
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz...");
+        System.out.println("Por favor, deixe sua mensagem após o sinal...");
+    }
 }
