0e825c0d7c9bebfa3c84e5e44afcb061af388811
laiscrz <lais.albino2003@gmail.com>
2024-06-28 23:46:48 -0300

    feat: update package 'componentes'

--- Challenges/JavaPOOChallenges/iPhoneModelingUML/ProjectIPhoneModel/src/br/com/iphone/componentes/ComponenteWeb.java
+++ Challenges/JavaPOOChallenges/iPhoneModelingUML/ProjectIPhoneModel/src/br/com/iphone/componentes/ComponenteWeb.java
@@ -2,4 +2,9 @@ package br.com.iphone.componentes;
 
 public abstract class ComponenteWeb {
 
+    public abstract void exibirPagina(String url);
+
+    public abstract void adicionarNovaAba();
+
+    public abstract void atualizarPagina();
 }
--- Challenges/JavaPOOChallenges/iPhoneModelingUML/ProjectIPhoneModel/src/br/com/iphone/componentes/NavegadorInternet.java
+++ Challenges/JavaPOOChallenges/iPhoneModelingUML/ProjectIPhoneModel/src/br/com/iphone/componentes/NavegadorInternet.java
@@ -1,5 +1,22 @@
-package br.com.iphone.dispositivos;
+package br.com.iphone.componentes;
 
-public class NavegadorInternet {
+public class NavegadorInternet extends ComponenteWeb {
 
+    @Override
+    public void exibirPagina(String url) {
+        System.out.println("Abrindo página: " + url);
+        System.out.println("Página aberta com sucesso!");
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Nova aba adicionada");
+        System.out.println("A nova aba está pronta para uso!");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Atualizando página atual...");
+        System.out.println("Página atualizada com novos conteúdos!");
+    }
 }
