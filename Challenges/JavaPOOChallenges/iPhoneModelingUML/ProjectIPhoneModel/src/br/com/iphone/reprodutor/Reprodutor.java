2829833cb054581cb938176c0d9109262e3e1081
laiscrz <lais.albino2003@gmail.com>
2024-06-28 23:58:03 -0300

    feat: update package 'reprodutor'

--- Challenges/JavaPOOChallenges/iPhoneModelingUML/ProjectIPhoneModel/src/br/com/iphone/reprodutor/Reprodutor.java
+++ Challenges/JavaPOOChallenges/iPhoneModelingUML/ProjectIPhoneModel/src/br/com/iphone/reprodutor/Reprodutor.java
@@ -2,4 +2,9 @@ package br.com.iphone.reprodutor;
 
 public interface Reprodutor {
 
+    void tocar();
+
+    void pausar();
+
+    void selecionarMusica(String musica);
 }
--- Challenges/JavaPOOChallenges/iPhoneModelingUML/ProjectIPhoneModel/src/br/com/iphone/reprodutor/ReprodutorMusical.java
+++ Challenges/JavaPOOChallenges/iPhoneModelingUML/ProjectIPhoneModel/src/br/com/iphone/reprodutor/ReprodutorMusical.java
@@ -1,5 +1,22 @@
 package br.com.iphone.reprodutor;
 
-public class ReprodutorMusical {
+public class ReprodutorMusical implements Reprodutor {
 
+    @Override
+    public void tocar() {
+        System.out.println("Iniciando reprodução...");
+        System.out.println("Música tocando.");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Pausando reprodução...");
+        System.out.println("Música pausada.");
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        System.out.println("Selecionando música...");
+        System.out.println("Música selecionada: " + musica);
+    }
 }
