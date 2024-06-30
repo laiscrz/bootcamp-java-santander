c72ae596b2bb05573cc16568b6ff40c6a3d9074e
laiscrz <lais.albino2003@gmail.com>
2024-06-30 14:15:11 -0300

    chore: Initial commit for ContextualizationStream project

---
+++ Content/JavaPOO/StreamAPI/ContextualizationStream/src/main/java/imperativeDeclarativeProgramming/Imperative.java
@@ -0,0 +1,9 @@
+package main.java.imperativeDeclarativeProgramming;
+
+
+
+// PROGRAMAÇÃO IMPERATIVA ->  paradigma em que o código descreve passo a passo como alcançar um resultado.
+public class Imperative {
+
+   
+}
