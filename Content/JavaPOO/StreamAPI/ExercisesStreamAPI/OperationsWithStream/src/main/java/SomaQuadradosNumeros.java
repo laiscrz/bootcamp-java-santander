149bebc1ca82d1353caf01239db8c086de8c1cfb
laiscrz <lais.albino2003@gmail.com>
2024-07-02 11:39:54 -0300

    feat: exercise 11 on StreamAPI done

--- Content/JavaPOO/StreamAPI/ExercisesStreamAPI/OperationsWithStream/src/main/java/SomaQuadradosNumeros.java
+++ Content/JavaPOO/StreamAPI/ExercisesStreamAPI/OperationsWithStream/src/main/java/SomaQuadradosNumeros.java
@@ -1,5 +1,18 @@
 package main.java;
 
-public class SomaQuadradosNumeros {
+import java.util.Arrays;
+import java.util.List;
 
+public class SomaQuadradosNumeros {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        // Calcula a soma dos quadrados dos números da lista
+        int somaQuadrados = numeros.stream()
+                .mapToInt(numero -> numero * numero) // Calcula o quadrado de cada número
+                .sum(); // Soma os quadrados
+
+        System.out.println("A soma dos quadrados dos números é: " + somaQuadrados);
+
+    }
 }
