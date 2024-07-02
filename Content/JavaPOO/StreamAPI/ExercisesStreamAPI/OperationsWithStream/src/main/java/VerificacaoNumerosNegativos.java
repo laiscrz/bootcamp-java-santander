884a2a3c5da0e3af18e34d98226376c5f5742921
laiscrz <lais.albino2003@gmail.com>
2024-07-02 13:38:15 -0300

    feat: exercise 15 on StreamAPI done

--- Content/JavaPOO/StreamAPI/ExercisesStreamAPI/OperationsWithStream/src/main/java/VerificacaoNumerosNegativos.java
+++ Content/JavaPOO/StreamAPI/ExercisesStreamAPI/OperationsWithStream/src/main/java/VerificacaoNumerosNegativos.java
@@ -1,5 +1,16 @@
 package main.java;
 
-public class VerificacaoNumerosNegativos {
+import java.util.Arrays;
+import java.util.List;
 
+public class VerificacaoNumerosNegativos {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        // Verifica se a lista contém pelo menos um número negativo
+        boolean contemAlgumNegativo = numeros.stream()
+                .anyMatch(n -> n < 0);
+
+        System.out.println("Contém pelo menos um número negativo? " + contemAlgumNegativo);
+    }
 }
