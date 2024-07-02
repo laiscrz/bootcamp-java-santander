6f3f135ac1d20be95ede24d845f772be19549973
laiscrz <lais.albino2003@gmail.com>
2024-07-02 13:45:58 -0300

    feat: exercise 16 on StreamAPI done

--- Content/JavaPOO/StreamAPI/ExercisesStreamAPI/OperationsWithStream/src/main/java/AgrupamentoParesImpares.java
+++ Content/JavaPOO/StreamAPI/ExercisesStreamAPI/OperationsWithStream/src/main/java/AgrupamentoParesImpares.java
@@ -1,5 +1,25 @@
 package main.java;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class AgrupamentoParesImpares {
 
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        // Agrupamento dos números em pares e ímpares
+        Map<Boolean, List<Integer>> numerosAgrupados = numeros.stream()
+                .collect(Collectors.partitioningBy(n -> n % 2 == 0));
+
+        // Listas de pares e ímpares
+        List<Integer> pares = numerosAgrupados.get(true);
+        List<Integer> impares = numerosAgrupados.get(false);
+
+        // Exibição dos resultados
+        System.out.println("Números Pares: " + pares);
+        System.out.println("Números Ímpares: " + impares);
+    }
 }
