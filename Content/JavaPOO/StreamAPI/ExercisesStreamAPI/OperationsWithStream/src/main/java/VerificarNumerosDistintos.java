6d324ec6240899404b4790f5126a4ec20bf0e094
laiscrz <lais.albino2003@gmail.com>
2024-07-02 11:28:55 -0300

    feat: exercise 9 on StreamAPI done

--- Content/JavaPOO/StreamAPI/ExercisesStreamAPI/OperationsWithStream/src/main/java/VerificarNumerosDistintos.java
+++ Content/JavaPOO/StreamAPI/ExercisesStreamAPI/OperationsWithStream/src/main/java/VerificarNumerosDistintos.java
@@ -1,5 +1,18 @@
 package main.java;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class VerificarNumerosDistintos {
 
+    public static void main(String[] args) {
+
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        // Verifica se há números distintos na lista
+        boolean numDistintos = numeros.stream().distinct().count() < numeros.size();
+
+        System.out.println("Existem números distintos na lista? " + numDistintos);
+    }
 }
+
