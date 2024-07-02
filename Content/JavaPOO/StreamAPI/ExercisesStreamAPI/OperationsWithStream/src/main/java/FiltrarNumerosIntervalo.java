52c6f94b70b1681da2b98d5318961102211bf69e
laiscrz <lais.albino2003@gmail.com>
2024-07-02 13:22:53 -0300

    feat: exercise 13 on StreamAPI done

--- Content/JavaPOO/StreamAPI/ExercisesStreamAPI/OperationsWithStream/src/main/java/FiltrarNumerosIntervalo.java
+++ Content/JavaPOO/StreamAPI/ExercisesStreamAPI/OperationsWithStream/src/main/java/FiltrarNumerosIntervalo.java
@@ -1,5 +1,23 @@
 package main.java;
 
-public class FiltrarNumerosIntervalo {
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
+public class FiltrarNumerosIntervalo {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+
+        // Define o intervalo desejado
+        int min = 5;
+        int max = 10;
+
+        // Filtra os números no intervalo específico
+        List<Integer> numerosFiltrados = numeros.stream()
+                .filter(num -> num >= min && num <= max)
+                .collect(Collectors.toList());
+
+        // Exibe o resultado no console
+        System.out.println("Números no intervalo [" + min + ", " + max + "]: " + numerosFiltrados);
+    }
 }
