5be4bac4be7d74f1bc68eceacd41593b33fd674b
laiscrz <lais.albino2003@gmail.com>
2024-07-02 13:55:32 -0300

    feat: exercise 18 on StreamAPI done

--- Content/JavaPOO/StreamAPI/ExercisesStreamAPI/OperationsWithStream/src/main/java/VerificacaoNumerosIguais.java
+++ Content/JavaPOO/StreamAPI/ExercisesStreamAPI/OperationsWithStream/src/main/java/VerificacaoNumerosIguais.java
@@ -1,5 +1,15 @@
 package main.java;
 
-public class VerificacaoNumerosIguais {
+import java.util.Arrays;
+import java.util.List;
 
+public class VerificacaoNumerosIguais {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        // Verifica se todos os números da lista são iguais
+        boolean todosIguais = numeros.stream().allMatch(n -> n.equals(numeros.get(0)));
+
+        System.out.println("Todos são iguais? " + todosIguais);
+    }
 }
