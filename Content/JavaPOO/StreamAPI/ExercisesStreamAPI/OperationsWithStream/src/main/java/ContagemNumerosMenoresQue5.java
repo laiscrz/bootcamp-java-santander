e14178d6b7e97a29994fca87f3ab08e930e9534c
laiscrz <lais.albino2003@gmail.com>
2024-07-02 14:00:22 -0300

    feat: exercise 20 on StreamAPI done

--- Content/JavaPOO/StreamAPI/ExercisesStreamAPI/OperationsWithStream/src/main/java/ContagemNumerosMenoresQue5.java
+++ Content/JavaPOO/StreamAPI/ExercisesStreamAPI/OperationsWithStream/src/main/java/ContagemNumerosMenoresQue5.java
@@ -1,5 +1,18 @@
 package main.java;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class ContagemNumerosMenoresQue5 {
 
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        // Filtrando os números menores que 5 e coletando-os em uma nova lista
+        List<Integer> numerosMenoresQue5 = numeros.stream()
+                .filter(n -> n < 5)
+                .toList(); // usando .toList() para coletar em uma lista
+
+        System.out.println("Números menores que 5 : " + numerosMenoresQue5);
+    }
 }
