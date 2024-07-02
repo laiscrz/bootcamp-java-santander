18c9e3100b5ebdfa44b638d8c6462ab227d97ae4
laiscrz <lais.albino2003@gmail.com>
2024-07-02 13:57:58 -0300

    feat: exercise 19 on StreamAPI done

--- Content/JavaPOO/StreamAPI/ExercisesStreamAPI/OperationsWithStream/src/main/java/SomaNumerosDivisiveis3e5.java
+++ Content/JavaPOO/StreamAPI/ExercisesStreamAPI/OperationsWithStream/src/main/java/SomaNumerosDivisiveis3e5.java
@@ -1,5 +1,19 @@
 package main.java;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class SomaNumerosDivisiveis3e5 {
 
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        // Encontra a soma dos números divisíveis por 3 e 5
+        int soma = numeros.stream()
+                .filter(n -> n % 3 == 0 && n % 5 == 0) // Filtra números divisíveis por 3 e 5
+                .mapToInt(Integer::intValue) // Converte para int
+                .sum(); // Calcula a soma
+
+        System.out.println("A soma dos números divisíveis por 3 e 5 é: " + soma);
+    }
 }
