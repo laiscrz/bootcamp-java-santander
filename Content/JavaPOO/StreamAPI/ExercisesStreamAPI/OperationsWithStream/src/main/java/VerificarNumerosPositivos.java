6b54ba90b6463a6e1dc41b67d502aeaf22e265c4
laiscrz <lais.albino2003@gmail.com>
2024-07-02 10:25:08 -0300

    feat: exercise 3 on StreamAPI done

--- Content/JavaPOO/StreamAPI/ExercisesStreamAPI/OperationsWithStream/src/main/java/VerificarNumerosPositivos.java
+++ Content/JavaPOO/StreamAPI/ExercisesStreamAPI/OperationsWithStream/src/main/java/VerificarNumerosPositivos.java
@@ -1,5 +1,20 @@
 package main.java;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class VerificarNumerosPositivos {
 
+    public static void main(String[] args) {
+
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        // allMatch() -> usado para verificar se todos os elementos de um stream
+        // correspondem a um determinado predicado.
+        boolean todosPositivos = numeros.stream()
+                .allMatch(n -> n > 0);
+
+        System.out.println("Todos os números são positivos? " + todosPositivos);
+
+    }
 }
