fae2db513c6d31a9e884c2c71fc773eef5951486
laiscrz <lais.albino2003@gmail.com>
2024-07-01 22:14:01 -0300

    feat: exercise 1 on StreamAPI done

--- Content/JavaPOO/StreamAPI/ExercisesStreamAPI/OperationsWithStream/src/main/java/OrdenarListaNumerica.java
+++ Content/JavaPOO/StreamAPI/ExercisesStreamAPI/OperationsWithStream/src/main/java/OrdenarListaNumerica.java
@@ -1,5 +1,13 @@
 package main.java;
 
-public class OrdenarListaNumerica {
+import java.util.Arrays;
+import java.util.List;
 
+public class OrdenarListaNumerica {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        // Utilizando Stream API para ordenar em ordem crescente
+        numeros.stream().sorted().forEach(System.out::println);
+    }
 }
