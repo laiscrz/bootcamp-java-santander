993b4ffe86ba8eebbfbe9ded9e545ecc50dc997d
laiscrz <lais.albino2003@gmail.com>
2024-07-03 20:53:33 -0300

    feat: update classes

--- Challenges/JavaPOOChallenges/BootcampPOO/src/br/com/poo/model/Mentoria.java
+++ Challenges/JavaPOOChallenges/BootcampPOO/src/br/com/poo/model/Mentoria.java
@@ -1,5 +1,29 @@
 package br.com.poo.model;
 
-public class Mentoria {
+import java.time.LocalDate;
 
+public class Mentoria extends Conteudo {
+
+    private LocalDate data;
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO + 20d;
+    }
+
+    public Mentoria() {
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria = {Título: '" + getTitulo() + "', descrição: '" + getDescricao() + "', data: " + data + "}";
+    }
 }
