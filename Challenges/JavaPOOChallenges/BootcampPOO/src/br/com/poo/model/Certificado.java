83d95204eb0a189096e427c0a26f425183bbace1
laiscrz <lais.albino2003@gmail.com>
2024-07-03 21:09:31 -0300

    feat: update classes 'Certificado'

--- Challenges/JavaPOOChallenges/BootcampPOO/src/br/com/poo/model/Certificado.java
+++ Challenges/JavaPOOChallenges/BootcampPOO/src/br/com/poo/model/Certificado.java
@@ -1,5 +1,47 @@
 package br.com.poo.model;
 
-public class Certificado {
+import java.time.LocalDate;
 
+public class Certificado {
+    private Dev dev;
+    private Curso curso;
+    private LocalDate dataConclusao;
+
+    public Certificado(Dev dev, Curso curso, LocalDate dataConclusao) {
+        this.dev = dev;
+        this.curso = curso;
+        this.dataConclusao = dataConclusao;
+    }
+
+    public Dev getDev() {
+        return dev;
+    }
+
+    public void setDev(Dev dev) {
+        this.dev = dev;
+    }
+
+    public Curso getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
+
+    public LocalDate getDataConclusao() {
+        return dataConclusao;
+    }
+
+    public void setDataConclusao(LocalDate dataConclusao) {
+        this.dataConclusao = dataConclusao;
+    }
+
+    @Override
+    public String toString() {
+        return "Certificado = { DEV: " + dev.getNome() +
+                ", curso: " + curso.getTitulo() +
+                ", dataConclusao: " + dataConclusao +
+                '}';
+    }
 }
