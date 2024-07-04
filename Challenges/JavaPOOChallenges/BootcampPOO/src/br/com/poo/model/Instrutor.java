0b12b56a339a639ff19005a7543d1e96f2aa5395
laiscrz <lais.albino2003@gmail.com>
2024-07-03 21:04:55 -0300

    feat: update classes 'Instrutor' and 'Usuario'

--- Challenges/JavaPOOChallenges/BootcampPOO/src/br/com/poo/model/Instrutor.java
+++ Challenges/JavaPOOChallenges/BootcampPOO/src/br/com/poo/model/Instrutor.java
@@ -1,5 +1,24 @@
 package br.com.poo.model;
 
-public class Instrutor {
+import java.util.LinkedHashSet;
+import java.util.Set;
 
+public class Instrutor extends Usuario {
+    private Set<Conteudo> conteudosMinistrados = new LinkedHashSet<>();
+
+    public Instrutor(String nome) {
+        super(nome);
+    }
+
+    public Set<Conteudo> getConteudosMinistrados() {
+        return conteudosMinistrados;
+    }
+
+    public void adicionarConteudoMinistrado(Conteudo conteudo) {
+        conteudosMinistrados.add(conteudo);
+    }
+
+    public void removerConteudoMinistrado(Conteudo conteudo) {
+        conteudosMinistrados.remove(conteudo);
+    }
 }
--- Challenges/JavaPOOChallenges/BootcampPOO/src/br/com/poo/model/Usuario.java
+++ Challenges/JavaPOOChallenges/BootcampPOO/src/br/com/poo/model/Usuario.java
@@ -1,5 +1,17 @@
 package br.com.poo.model;
 
 public class Usuario {
+    private String nome;
 
+    public Usuario(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
