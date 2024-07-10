7b78703a3e2ed0b6c8799cafb722f5c721329f44
Laís Cruz <133046134+laiscrz@users.noreply.github.com>
2024-07-10 16:23:32 -0300

    feat: create deletes.sql

---
+++ Content/JavaDataBase/SQLFundamentals/DatabaseModeling/Scripts/deletes.sql
@@ -0,0 +1,7 @@
+-- DELETE
+
+-- Deleta todos os registros da tabela `reservas` onde o status é 'cancelada'
+DELETE FROM reservas WHERE status = 'cancelada';
+
+-- Deleta todos os registros da tabela `reservas`
+DELETE FROM reservas;
