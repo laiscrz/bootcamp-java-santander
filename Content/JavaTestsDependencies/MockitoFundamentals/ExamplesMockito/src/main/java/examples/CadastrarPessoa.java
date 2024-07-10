d364babd97984cf48162c2f1d4be765f4e343998
laiscrz <lais.albino2003@gmail.com>
2024-07-10 13:01:20 -0300

    chore: update name var

--- Content/JavaTestsDependencies/MockitoFundamentals/ExamplesMockito/src/main/java/examples/CadastrarPessoa.java
+++ Content/JavaTestsDependencies/MockitoFundamentals/ExamplesMockito/src/main/java/examples/CadastrarPessoa.java
@@ -3,15 +3,15 @@ package examples;
 import java.time.LocalDate;
 
 public class CadastrarPessoa {
-    private ApiCorreios apiDosCorreios;
+    private ApiCorreios apiCorreios;
 
-    public CadastrarPessoa(final ApiCorreios apiDosCorreios) {
-        this.apiDosCorreios = apiDosCorreios;
+    public CadastrarPessoa(final ApiCorreios apiCorreios) {
+        this.apiCorreios = apiCorreios;
     }
 
     public Pessoa cadastrarPessoa(String nome, String documento, LocalDate nascimento, String cep) {
         Pessoa pessoa = new Pessoa(nome, documento, nascimento);
-        DadosLocalizacao dadosLocalizacao = apiDosCorreios.buscaDadosComBaseNoCep(cep);
+        DadosLocalizacao dadosLocalizacao = apiCorreios.buscaDadosComBaseNoCep(cep);
         pessoa.adicionaDadosDeEndereco(dadosLocalizacao);
         return pessoa;
     }
