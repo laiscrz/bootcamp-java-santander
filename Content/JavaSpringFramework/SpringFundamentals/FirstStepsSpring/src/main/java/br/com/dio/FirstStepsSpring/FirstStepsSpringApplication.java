dbbf41eb8557bc5225a2c95311da27c2a25079bf
laiscrz <lais.albino2003@gmail.com>
2024-07-11 15:35:07 -0300

    chore: Initial commit for FirstStepsSpring project

---
+++ Content/JavaSpringFramework/SpringFundamentals/FirstStepsSpring/src/main/java/br/com/dio/FirstStepsSpring/FirstStepsSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.dio.FirstStepsSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstStepsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstStepsSpringApplication.class, args);
+	}
+
+}
---
+++ Content/JavaSpringFramework/SpringFundamentals/FirstStepsSpring/src/test/java/br/com/dio/FirstStepsSpring/FirstStepsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dio.FirstStepsSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstStepsSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
