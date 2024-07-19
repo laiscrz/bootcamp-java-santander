1b5b1009c1125150dbfdf8d18a6ee31ea6296789
laiscrz <lais.albino2003@gmail.com>
2024-07-19 19:18:03 -0300

    chore: Initial commit for SpringScopePrototype project

---
+++ Content/JavaSpringFramework/SpringFundamentals/SpringScopePrototype/src/main/java/br/com/dio/SpringScopePrototype/SpringScopePrototypeApplication.java
@@ -0,0 +1,13 @@
+package br.com.dio.SpringScopePrototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringScopePrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringScopePrototypeApplication.class, args);
+	}
+
+}
---
+++ Content/JavaSpringFramework/SpringFundamentals/SpringScopePrototype/src/test/java/br/com/dio/SpringScopePrototype/SpringScopePrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dio.SpringScopePrototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringScopePrototypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
