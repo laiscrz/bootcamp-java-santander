45ed2a8447880996d163be0134c9cac47476c077
laiscrz <lais.albino2003@gmail.com>
2024-07-19 20:00:01 -0300

    chore: update project

---
+++ Content/JavaSpringFramework/SpringFundamentals/SpringConfigurationProperties/src/test/java/br/com/dio/SpringConfigurationProperties/SpringPropertiesApplicationTests.java
@@ -0,0 +1,14 @@
+package br.com.dio.SpringConfigurationProperties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigurationPropertiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	
+	}
+
+}
