e6798ceb0d8d3efadc368f9faec682193a58d1ac
laiscrz <lais.albino2003@gmail.com>
2024-07-19 19:28:49 -0300

    feat: create and update classdone

--- Content/JavaSpringFramework/SpringFundamentals/SpringScopePrototype/src/main/java/br/com/dio/SpringScopePrototype/SpringScopePrototypeApplication.java
+++ Content/JavaSpringFramework/SpringFundamentals/SpringScopePrototype/src/main/java/br/com/dio/SpringScopePrototype/SpringScopePrototypeApplication.java
@@ -1,13 +1,26 @@
 package br.com.dio.SpringScopePrototype;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import br.com.dio.SpringScopePrototype.app.SistemaMensagem;
 
 @SpringBootApplication
 public class SpringScopePrototypeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringScopePrototypeApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner run(SistemaMensagem sistema) throws Exception {
+		return args -> {
+			sistema.enviarConfirmacaoCadastro();
+			sistema.enviarMensagemBoasVindas();
+			sistema.enviarConfirmacaoCadastro();
+		};
 	}
 
 }
