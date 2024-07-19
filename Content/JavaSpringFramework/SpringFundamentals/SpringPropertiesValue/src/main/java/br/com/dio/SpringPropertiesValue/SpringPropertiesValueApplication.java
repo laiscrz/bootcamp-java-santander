3234faf32fe9a699a519d51c09daa8a7d2e94c43
laiscrz <lais.albino2003@gmail.com>
2024-07-19 19:38:42 -0300

    chore: Initial commit for SpringPropertiesValue project

---
+++ Content/JavaSpringFramework/SpringFundamentals/SpringPropertiesValue/src/main/java/br/com/dio/SpringPropertiesValue/SpringPropertiesValueApplication.java
@@ -0,0 +1,13 @@
+package br.com.dio.SpringPropertiesValue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPropertiesValueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPropertiesValueApplication.class, args);
+	}
+
+}
