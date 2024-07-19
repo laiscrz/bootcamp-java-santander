9f7ce536f98171e611db9d52b89de60eab1ecb71
laiscrz <lais.albino2003@gmail.com>
2024-07-19 19:56:12 -0300

    chore: Initial commit for SpringConfigurationProperties project

---
+++ Content/JavaSpringFramework/SpringFundamentals/SpringConfigurationProperties/src/main/java/br/com/dio/SpringConfigurationProperties/SpringConfigurationPropertiesApplication.java
@@ -0,0 +1,13 @@
+package br.com.dio.SpringConfigurationProperties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConfigurationPropertiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConfigurationPropertiesApplication.class, args);
+	}
+
+}
