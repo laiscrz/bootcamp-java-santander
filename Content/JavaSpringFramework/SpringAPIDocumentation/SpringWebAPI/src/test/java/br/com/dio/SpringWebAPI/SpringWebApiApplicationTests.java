78cd85f260d544eccebd0494bdbeafa000b853ae
laiscrz <lais.albino2003@gmail.com>
2024-07-19 22:34:17 -0300

    chore: Initial commit for SpringWebAPI project

---
+++ Content/JavaSpringFramework/SpringAPIDocumentation/SpringWebAPI/src/test/java/br/com/dio/SpringWebAPI/SpringWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dio.SpringWebAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
