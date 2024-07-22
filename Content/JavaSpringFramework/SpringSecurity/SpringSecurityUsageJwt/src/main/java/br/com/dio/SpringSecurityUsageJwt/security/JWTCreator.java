07c73c97e5025f26186472bc0830137112dcd4b4
laiscrz <lais.albino2003@gmail.com>
2024-07-22 15:01:02 -0300

    feat: create class in package 'security'

---
+++ Content/JavaSpringFramework/SpringSecurity/SpringSecurityUsageJwt/src/main/java/br/com/dio/SpringSecurityUsageJwt/security/JWTCreator.java
@@ -0,0 +1,51 @@
+package br.com.dio.SpringSecurityUsageJwt.security;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
+
+public class JWTCreator {
+    public static final String HEADER_AUTHORIZATION = "Authorization";
+    public static final String ROLES_AUTHORITIES = "authorities";
+
+    public static String create(String prefix, String key, JWTObject jwtObject) {
+        String token = Jwts.builder()
+                .setSubject(jwtObject.getSubject())
+                .setIssuedAt(jwtObject.getIssuedAt())
+                .setExpiration(jwtObject.getExpiration())
+                .claim(ROLES_AUTHORITIES, checkRoles(jwtObject.getRoles()))
+                .signWith(SignatureAlgorithm.HS512, key)
+                .compact();
+        return prefix + " " + token;
+    }
+
+    @SuppressWarnings("unchecked") // Adiciona a anotação para suprimir o aviso de tipo não verificado
+    public static JWTObject create(String token, String prefix, String key)
+            throws ExpiredJwtException, UnsupportedJwtException, MalformedJwtException, SignatureException {
+        JWTObject object = new JWTObject();
+        token = token.replace(prefix, "");
+        Claims claims = Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody();
+        object.setSubject(claims.getSubject());
+        object.setExpiration(claims.getExpiration());
+        object.setIssuedAt(claims.getIssuedAt());
+
+        // Casting seguro para List<String>
+        List<String> roles = (List<String>) claims.get(ROLES_AUTHORITIES);
+        object.setRoles(roles);
+
+        return object;
+    }
+
+    private static List<String> checkRoles(List<String> roles) {
+        return roles.stream()
+                .map(s -> "ROLE_".concat(s.replaceAll("ROLE_", "")))
+                .collect(Collectors.toList());
+    }
+}
---
+++ Content/JavaSpringFramework/SpringSecurity/SpringSecurityUsageJwt/src/main/java/br/com/dio/SpringSecurityUsageJwt/security/WebSecurityConfig.java
@@ -0,0 +1,76 @@
+package br.com.dio.SpringSecurityUsageJwt.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+// import org.h2.server.web.WebServlet;
+// import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    private static final String[] SWAGGER_WHITELIST = {
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**"
+    };
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(withDefaults())
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers(SWAGGER_WHITELIST).permitAll()
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/users").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/users").hasAnyRole("USERS", "MANAGERS")
+                        .requestMatchers("/managers").hasRole("MANAGERS")
+                        .anyRequest().authenticated())
+                .addFilterAfter(new JWTFilter(), UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement(sessionManagement -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    /*@Bean // Habilitando o acesso ao H2 Database na web
+    public ServletRegistrationBean<WebServlet> h2servletRegistration() {
+        ServletRegistrationBean<WebServlet> registrationBean = new ServletRegistrationBean<>(new WebServlet(), "/h2-console/*");
+        registrationBean.setName("H2Console");
+        return registrationBean;
+    }*/
+}
