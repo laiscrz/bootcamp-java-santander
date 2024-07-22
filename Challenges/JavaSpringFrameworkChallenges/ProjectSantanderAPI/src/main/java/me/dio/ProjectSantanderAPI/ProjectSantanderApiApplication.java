38466a8645357fb733a49c7838d14b53ef513839
laiscrz <lais.albino2003@gmail.com>
2024-07-22 18:43:17 -0300

    chore: Initial commit for ProjectSantanderAPI project

---
+++ Challenges/JavaSpringFrameworkChallenges/ProjectSantanderAPI/src/main/java/me/dio/ProjectSantanderAPI/ProjectSantanderApiApplication.java
@@ -0,0 +1,13 @@
+package me.dio.ProjectSantanderAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSantanderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSantanderApiApplication.class, args);
+	}
+
+}
