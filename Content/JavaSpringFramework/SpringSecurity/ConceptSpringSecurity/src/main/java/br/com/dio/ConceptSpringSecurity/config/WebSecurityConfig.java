08b7824ce3a79596e68ab9698cf711b6d879ee31
laiscrz <lais.albino2003@gmail.com>
2024-07-22 12:57:33 -0300

    feat: create project structure

---
+++ Content/JavaSpringFramework/SpringSecurity/ConceptSpringSecurity/src/main/java/br/com/dio/ConceptSpringSecurity/config/WebSecurityConfig.java
@@ -0,0 +1,5 @@
+package br.com.dio.ConceptSpringSecurity.config;
+
+public class WebSecurityConfig {
+
+}
---
+++ Content/JavaSpringFramework/SpringSecurity/ConceptSpringSecurity/src/main/java/br/com/dio/ConceptSpringSecurity/repository/IUserRepository.java
@@ -0,0 +1,12 @@
+package br.com.dio.ConceptSpringSecurity.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import br.com.dio.ConceptSpringSecurity.model.User;
+
+public interface IUserRepository extends JpaRepository<User, Integer> {
+    @Query("SELECT e FROM User e JOIN FETCH e.roles WHERE e.username= (:username)")
+    public User findByUsername(@Param("username") String username);
+}
