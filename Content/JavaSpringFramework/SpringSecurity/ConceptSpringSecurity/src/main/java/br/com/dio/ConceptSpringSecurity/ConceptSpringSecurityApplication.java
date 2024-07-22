e53c4954b0319ff2a57fe4c611be216ef6b568ba
laiscrz <lais.albino2003@gmail.com>
2024-07-22 12:48:21 -0300

    chore: Initial commit for ConceptSpringSecurity project

---
+++ Content/JavaSpringFramework/SpringSecurity/ConceptSpringSecurity/src/main/java/br/com/dio/ConceptSpringSecurity/ConceptSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package br.com.dio.ConceptSpringSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConceptSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConceptSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ Content/JavaSpringFramework/SpringSecurity/ConceptSpringSecurity/src/test/java/br/com/dio/ConceptSpringSecurity/ConceptSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dio.ConceptSpringSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConceptSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
