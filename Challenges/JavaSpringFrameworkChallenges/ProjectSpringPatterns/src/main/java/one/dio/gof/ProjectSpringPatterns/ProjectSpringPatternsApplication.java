1e819dccc6114ce1c0cc21db877e09227561f91e
laiscrz <lais.albino2003@gmail.com>
2024-07-22 16:13:51 -0300

    chore: Initial commit for ProjectSpringPatterns project

---
+++ Challenges/JavaSpringFrameworkChallenges/ProjectSpringPatterns/src/main/java/one/dio/gof/ProjectSpringPatterns/ProjectSpringPatternsApplication.java
@@ -0,0 +1,13 @@
+package one.dio.gof.ProjectSpringPatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpringPatternsApplication.class, args);
+	}
+
+}
---
+++ Challenges/JavaSpringFrameworkChallenges/ProjectSpringPatterns/src/test/java/one/dio/gof/ProjectSpringPatterns/ProjectSpringPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package one.dio.gof.ProjectSpringPatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
