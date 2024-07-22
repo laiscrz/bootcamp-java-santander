de74d0fe5e2815ad5e1e409c036982e57a61ba00
laiscrz <lais.albino2003@gmail.com>
2024-07-22 17:01:19 -0300

    chore: update class Application project

--- Challenges/JavaSpringFrameworkChallenges/ProjectSpringPatterns/src/main/java/one/dio/gof/ProjectSpringPatterns/ProjectSpringPatternsApplication.java
+++ Challenges/JavaSpringFrameworkChallenges/ProjectSpringPatterns/src/main/java/one/dio/gof/ProjectSpringPatterns/ProjectSpringPatternsApplication.java
@@ -2,7 +2,19 @@ package one.dio.gof.ProjectSpringPatterns;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/**
+ * Projeto Spring Boot gerado via Spring Initializr.
+ * Os seguintes módulos foram selecionados:
+ * - Spring Data JPA
+ * - Spring Web
+ * - H2 Database
+ * - OpenFeign
+ * 
+ * @author lais
+ */
+@EnableFeignClients
 @SpringBootApplication
 public class ProjectSpringPatternsApplication {
 
