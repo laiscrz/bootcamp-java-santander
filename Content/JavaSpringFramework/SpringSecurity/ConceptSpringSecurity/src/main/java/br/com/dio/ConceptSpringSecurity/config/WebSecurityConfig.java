edc06431800110e8259d23f3aa0245fd61282698
laiscrz <lais.albino2003@gmail.com>
2024-07-22 13:33:38 -0300

    feat: update WebSecurityConfig class

--- Content/JavaSpringFramework/SpringSecurity/ConceptSpringSecurity/src/main/java/br/com/dio/ConceptSpringSecurity/config/WebSecurityConfig.java
+++ Content/JavaSpringFramework/SpringSecurity/ConceptSpringSecurity/src/main/java/br/com/dio/ConceptSpringSecurity/config/WebSecurityConfig.java
@@ -1,5 +1,31 @@
 package br.com.dio.ConceptSpringSecurity.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig {
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                .requestMatchers("/").permitAll()
+                .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                .requestMatchers("/managers").hasAnyRole("MANAGERS")
+                .requestMatchers("/users").hasAnyRole("USERS", "MANAGERS")
+                .anyRequest().authenticated()
+            )
+            .httpBasic(withDefaults());
+        return http.build();
+    }
 }
